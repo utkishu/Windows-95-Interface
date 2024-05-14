26ea75899bd59f22f11b3bc46618565780a0a8aa
utkishu <152878833+utkishu@users.noreply.github.com>
2024-05-14 15:06:03 +0100

    Got letterbox view working

--- CU4012-SFML/Main.cpp
+++ CU4012-SFML/Main.cpp
@@ -43,108 +43,98 @@ sf::View getLetterboxView(sf::View view, int windowWidth, int windowHeight) {
 	return view;
 
 }
-void windowProcess(sf::RenderWindow* window, Input* in,sf::View view)
+void windowProcess(sf::RenderWindow* window, Input* in, sf::View& view)
 {
-	// Handle window events.
-	sf::Event event;
+    sf::Event event;
 
-
-	while (window->pollEvent(event))
-	{
-
-		switch (event.type)
-		{
-		case sf::Event::Closed:
-			window->close();
-			break;
-		case sf::Event::Resized:
-			window->setView(sf::View(sf::FloatRect(0.f, 0.f, (float)event.size.width, (float)event.size.height)));
-			view = getLetterboxView(view, event.size.width, event.size.height);
-			//window->setView(sf::View(sf::FloatRect(0.f, 0.f, (float)event.size.width, (float)event.size.height)));
-			break;
-		case sf::Event::KeyPressed:
-			// update input class
-			in->setKeyDown(event.key.code);
-			break;
-		case sf::Event::KeyReleased:
-			//update input class
-			in->setKeyUp(event.key.code);
-			break;
-		case sf::Event::MouseMoved:
-			//update input class
-			in->setMousePosition(event.mouseMove.x, event.mouseMove.y);
-			break;
-		case sf::Event::MouseButtonPressed:
-			if (event.mouseButton.button == sf::Mouse::Left)
-			{
-				//update input class
-				in->setLeftMouse(Input::MouseState::DOWN);
-			}
-			else if (event.mouseButton.button == sf::Mouse::Right)
-			{
-				in->setRightMouse(Input::MouseState::DOWN);
-			}
-			break;
-		case sf::Event::MouseButtonReleased:
-			if (event.mouseButton.button == sf::Mouse::Left)
-			{
-				//update input class
-				in->setLeftMouse(Input::MouseState::UP);
-			}
-			else if (event.mouseButton.button == sf::Mouse::Right)
-			{
-				in->setRightMouse(Input::MouseState::UP);
-			}
-			break;
-		default:
-			// don't handle other events
-			break;
-		}
-	}
+    while (window->pollEvent(event))
+    {
+        switch (event.type)
+        {
+        case sf::Event::Closed:
+            window->close();
+            break;
+        case sf::Event::Resized:
+            // Update the view to maintain the letterbox effect
+            view = getLetterboxView(view, event.size.width, event.size.height);
+            break;
+        case sf::Event::KeyPressed:
+            in->setKeyDown(event.key.code);
+            break;
+        case sf::Event::KeyReleased:
+            in->setKeyUp(event.key.code);
+            break;
+        case sf::Event::MouseMoved:
+            in->setMousePosition(event.mouseMove.x, event.mouseMove.y);
+            break;
+        case sf::Event::MouseButtonPressed:
+            if (event.mouseButton.button == sf::Mouse::Left)
+            {
+                in->setLeftMouse(Input::MouseState::DOWN);
+            }
+            else if (event.mouseButton.button == sf::Mouse::Right)
+            {
+                in->setRightMouse(Input::MouseState::DOWN);
+            }
+            break;
+        case sf::Event::MouseButtonReleased:
+            if (event.mouseButton.button == sf::Mouse::Left)
+            {
+                in->setLeftMouse(Input::MouseState::UP);
+            }
+            else if (event.mouseButton.button == sf::Mouse::Right)
+            {
+                in->setRightMouse(Input::MouseState::UP);
+            }
+            break;
+        default:
+            break;
+        }
+    }
 }
-
 
 int main()
 {
-	sf::View view;
+    sf::View view;
 
-	// Create the main window with a fixed size
-	sf::RenderWindow window(sf::VideoMode(800, 600), "WindowsXP",sf::Style::Fullscreen);
+    // Create the main window with a fixed size
+    sf::RenderWindow window(sf::VideoMode(800, 600), "WindowsXP", sf::Style::Fullscreen);
 
-	view.setSize(282, 282);
-	view.setCenter(view.getSize().x / 2, view.getSize().y / 2);
-	view = getLetterboxView(view, 800, 600);
+    // Initialize the view with a fixed size and center
+    view.setSize(282, 282);
+    view.setCenter(view.getSize().x / 2, view.getSize().y / 2);
 
+    // Set the initial letterbox view based on the window size
+    view = getLetterboxView(view, 800, 600);
 
-	// Initialise input and level objects.
-	Input input;
-	Level level(&window, &input);
+    // Initialise input and level objects.
+    Input input;
+    Level level(&window, &input);
 
-	// Initialise objects for delta time
-	sf::Clock clock;
-	float deltaTime;
+    // Initialise objects for delta time
+    sf::Clock clock;
+    float deltaTime;
 
+    // Game Loop
+    while (window.isOpen())
+    {
+        // Process window events
+        windowProcess(&window, &input, view);
 
+        // Calculate delta time
+        deltaTime = clock.restart().asSeconds();
 
-	// Game Loop
-	while (window.isOpen())
-	{
-		//Process window events
-		windowProcess(&window, &input,view);
+        // Call standard game loop functions (input, update and render)
+        level.handleInput(deltaTime);
+        level.update(deltaTime);
+        level.render();
 
-		// Calculate delta time. How much time has passed 
-		// since it was last calculated (in seconds) and restart the clock.
-		deltaTime = clock.restart().asSeconds();
+        // Update input class, handle pressed keys
+        // Must be done last.
+        input.update();
+    }
 
-		// Call standard game loop functions (input, update and render)
-		level.handleInput(deltaTime);
-		level.update(deltaTime);
-		level.render();
-
-		// Update input class, handle pressed keys
-		// Must be done last.
-		input.update();
-	}
-
+    return 0;
 }
+
 
